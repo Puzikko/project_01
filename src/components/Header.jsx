bb62d05c4e09c8a0084198f582c907fc669a8fc9
Puziko <112590501+Puzikko@users.noreply.github.com>
2022-09-20 22:19:26 +0300

    Lesson 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='head'>
+            <img src='https://w7.pngwing.com/pngs/741/880/png-transparent-feather-violet-quill-drawing-feather-purple-leaf-animals-thumbnail.png' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div><img src='https://nosomosnonos.com/wp-content/uploads/2021/11/WOW-Warcraft-World-of-Warcraft-1.jpg' /></div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My post
+        <div>
+          New post
+        </div>
+      </div>
+      <div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
