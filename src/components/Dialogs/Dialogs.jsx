e17368a75ac4d7b0473629603ea82d3032abc854
Puziko <112590501+Puzikko@users.noreply.github.com>
2022-09-30 09:18:20 +0300

    Lesson 21 Вёрстка Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,19 @@ import classDialogs from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div>
-            Dialogs
+            <div className={classDialogs.homePage}>
+                <div className={classDialogs.userDialogs}>
+                    <div className={classDialogs.dialog + ' ' + classDialogs.active}>Serega</div>
+                    <div className={classDialogs.dialog}>Valenta</div>
+                    <div className={classDialogs.dialog}>Antony</div>
+                    <div className={classDialogs.dialog}>Davy Jones</div>
+                    <div className={classDialogs.dialog}>Sharky</div>
+                </div>
+                <div className={classDialogs.userMessages}>
+                    <div>WTF?</div>
+                    <div>Wassssup?</div>
+                </div>
+            </div>
         </div>
     );
 }
