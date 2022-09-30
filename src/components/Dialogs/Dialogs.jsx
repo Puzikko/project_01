61b18ed0553306dccb0bbe75d68ddd8ab9cfe277
Puziko <112590501+Puzikko@users.noreply.github.com>
2022-09-30 19:54:25 +0300

    Lesson 22 Route exact, but at React V6 <Route to='/dialogs/*'> , вставляем звёздочку

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import classDialogs from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div>
             <div className={classDialogs.homePage}>
                 <div className={classDialogs.userDialogs}>
-                    <div className={classDialogs.dialog + ' ' + classDialogs.active}>Serega</div>
-                    <div className={classDialogs.dialog}>Valenta</div>
-                    <div className={classDialogs.dialog}>Antony</div>
-                    <div className={classDialogs.dialog}>Davy Jones</div>
-                    <div className={classDialogs.dialog}>Sharky</div>
+                    <div className={classDialogs.dialog}>
+                        <NavLink to='/dialogs/1'>Serega</NavLink>
+                    </div>
+                    <div className={classDialogs.dialog}>
+                        <NavLink to='/dialogs/2'>Valenta</NavLink>
+                        </div>
+                    <div className={classDialogs.dialog}>
+                        <NavLink to='/dialogs/3'>Antony</NavLink>
+                        </div>
+                    <div className={classDialogs.dialog}>
+                        <NavLink to='/dialogs/4'>Davy Jones</NavLink>
+                        </div>
+                    <div className={classDialogs.dialog}>
+                        <NavLink to='/dialogs/5'>Sharky</NavLink>
+                        </div>
                 </div>
                 <div className={classDialogs.userMessages}>
                     <div>WTF?</div>
