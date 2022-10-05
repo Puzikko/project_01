d8fc3e6390c2d81c8eb38ad74952440e76cac25c
Puziko <112590501+Puzikko@users.noreply.github.com>
2022-10-05 16:34:36 +0300

    Lesson 25 .map для диалогов, сообщений и постов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,30 +2,51 @@ import React from 'react';
 import classDialogs from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+const DialogUser = (props) => {
+    let path = '/dialogs/' + props.id
+    return (
+        <div className={classDialogs.dialog}>
+            <NavLink to={path}
+            className={({isActive}) => isActive ? classDialogs.active : ''}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div className={classDialogs.userMessages}>
+            <div>{props.message}</div>
+        </div>
+    )
+}
+
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Serega'},
+        {id: 2, name: 'Valenta'},
+        {id: 3, name: 'Arsenchik'},
+        {id: 4, name: 'Davy Johnes'},
+        {id: 5, name: 'Sharky'}
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Wassssup men?'},
+        {id: 1, message: 'How much is the fish?'},
+        {id: 1, message: 'Check the microphone!'}
+    ]
+
+    let dialog = dialogsData.map(dialog => <DialogUser name={dialog.name} id={dialog.id}/>)
+    let message = messagesData.map(message => <Message message={message.message}/>)
+
     return (
         <div>
             <div className={classDialogs.homePage}>
                 <div className={classDialogs.userDialogs}>
-                    <div className={classDialogs.dialog}>
-                        <NavLink to='/dialogs/1'>Serega</NavLink>
-                    </div>
-                    <div className={classDialogs.dialog}>
-                        <NavLink to='/dialogs/2'>Valenta</NavLink>
-                        </div>
-                    <div className={classDialogs.dialog}>
-                        <NavLink to='/dialogs/3'>Antony</NavLink>
-                        </div>
-                    <div className={classDialogs.dialog}>
-                        <NavLink to='/dialogs/4'>Davy Jones</NavLink>
-                        </div>
-                    <div className={classDialogs.dialog}>
-                        <NavLink to='/dialogs/5'>Sharky</NavLink>
-                        </div>
+                    { dialog }
                 </div>
-                <div className={classDialogs.userMessages}>
-                    <div>WTF?</div>
-                    <div>Wassssup?</div>
+                <div>
+                    { message }
                 </div>
             </div>
         </div>
