7296b1a4df8eacdcb71929fb39d90f412f9a7148
Ihar Zgeria <ihar.zgeria@gmail.com>
2023-03-19 19:23:17 +0300

    Lesson 97. Put new profile info on server

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getProfileUserData } from '../../redux/SettingsSelector';
+import SettingsReduxForm from './SettingsForm';
+import { saveProfileInfoThunk } from './../../redux/SettingsReducer';
+import { getUserId } from './../../redux/AuthSelector';
+import { WithAuthRedirect } from './../../hoc/withAuthRedirect';
+import { Navigate } from 'react-router-dom';
 
 const Settings = (props) => {
-    return (
-        <div>
-            Settings
-        </div>
-    );
+    // const [toggleFetch, setToggleFetch] = useState(false);
+    const { profileUserData, userId } = props;
+    // useEffect(() => {
+    //     debugger
+    //     toggleFetch &&
+    //         <Navigate replace to={'profile/' + userId} />;
+    //     setToggleFetch(false)
+    // }, [toggleFetch])
+    const onSubmit = (formData) => {
+        props.saveProfileInfoThunk(formData, userId)
+        // .then(() => {
+        //     setToggleFetch(true)
+        // })
+    };
+    // const toFn = () => {
+    //     debugger
+    //     return <Navigate replace to={'/dialogs/'} />
+    // };
+    return <>
+        {/* <button onClick={() => toFn()}>To</button> */}
+        <SettingsReduxForm initialValues={props.initialValues} onSubmit={onSubmit} profileUserData={profileUserData} props={props} />
+    </>
+};
+
+const SettingsWithAuthRedirect = WithAuthRedirect(Settings)
+
+const mapStateToProps = (state) => {
+    return {
+        profileUserData: getProfileUserData(state),
+        userId: getUserId(state),
+    }
 }
 
-export default Settings
+const SettingsContainer = connect(mapStateToProps, { saveProfileInfoThunk })(SettingsWithAuthRedirect)
+
+export default SettingsContainer;
